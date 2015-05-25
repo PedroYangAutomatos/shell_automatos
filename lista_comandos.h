5e1bcd6c8eb1bc5026c6290946e36bc6364f092c
Pedro Ardaglio de Mello <pedro@localhost.localdomain>
2015-05-24 22:12:56 -0300

    Estrutura de dados que guarda os comandos e parâmetros quase pronta, falta só ajeitar alguns detalhes.

---
+++ lista_comandos.h
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include "comando_params.h"
+
+using namespace std;
+
+class ListaComandos{
+    private:
+        std::vector<ComandoParams> comandos;
+    public:
+        ListaComandos(std::vector<ComandoParams>);
+        int procura_comando(std::string);
+        int tam();
+        void aumentar_tamanho();
+        // void adicionar_elemento(std::string);
+        ComandoParams busca_indice(int);
+        void adicionar_comando(std::string, std::string, std::vector<std::string>, std::vector<std::string>);
+};
+
+ListaComandos::ListaComandos(std::vector<ComandoParams> c){
+    comandos = c;
+}
+
+// Retorna o índice do comando na lista, ou -1 se não achou
+int ListaComandos::procura_comando(std::string comando){
+    int tam = ListaComandos::tam();
+    int indice_comando = -1;
+
+    if (tam > 0){
+        for(int i = 0; i = tam; i++){
+            indice_comando = comandos[i].verifica(comando);
+        }
+    }
+
+    // Se tem um índice para esse comando
+    if (indice_comando != -1){
+        return indice_comando;
+    }
+
+    return -1;
+}
+
+int ListaComandos::tam(){
+    return comandos.size();
+}
+
+// Aumenta o tamanho da lista de comandos em um elemento
+void ListaComandos::aumentar_tamanho(){
+    comandos.resize(comandos.size() + 1);
+}
+
+// void ListaComandos::adicionar_elemento(std::string elem){
+//    ListaComandos::aumentar_tamanho();
+//    comandos[comandos.size() + 1] = elem;
+// }
+
+ComandoParams ListaComandos::busca_indice(int indice){
+    if (ListaComandos::tam() < indice){
+        exit(1);
+    }
+    else{
+        return comandos[indice];
+    }
+}
+
+void ListaComandos::adicionar_comando(std::string cl, std::string cw, std::vector<std::string> pl, std::vector<std::string> pw){
+    ListaComandos::aumentar_tamanho();
+    int tamanho = ListaComandos::tam() - 1;
+
+    if (tamanho < 0){
+        return;
+    }
+
+    comandos[tamanho].adicionar(cl, cw, pl, pw);
+
+}
